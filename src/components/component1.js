2414fb4e02fff64debb5ce137a12f08a2928f70e
Aleksander <aleksandergorecki80@gmail.com>
2020-11-21 20:49:26 +0000

    Basic opacity animation

---
+++ src/components/component1.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Spring} from 'react-spring/renderprops';
+
+export default function Component1() {
+    return (
+        <Spring
+            from={{ opacity: 0, marginTop: -500 }}
+            to={{ opacity: 1, marginTop: 0 }}
+        >
+            {props => (
+                <div style={props}>
+                    <div style={c1Style}>
+                        <h1>Component 1</h1>
+                        <p>
+                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            </p>
+                    </div>
+                </div>
+            )}
+        </Spring>
+
+    )
+}
+
+const c1Style = {
+    background: '#6D6875',
+    color: 'white',
+    padding: '1.5rem'
+}
